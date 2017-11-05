221265f3b55a93eb03deae75adba15bb7daa8978
somil kumar <somil.kumar91@gmail.com>
2017-11-05 14:57:20 +0530

    Using Yargvs

--- notes.js
+++ notes.js
@@ -1,2 +1,26 @@
 console.log('Starting notes.js');
 
+
+const addNote = (title, body) => {
+    console.log('Adding Note: ', title, body);
+}
+
+const getAll = () => {
+    console.log('Getting all notes');
+}
+
+const readNote = (title) => {
+    console.log('Reading note: ', title);
+}
+
+const removeNote = (title) => {
+    console.log('Removing note: ', title);
+}
+
+
+module.exports = {
+    addNote,
+    getAll,
+    readNote,
+    removeNote
+};
