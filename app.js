c164040264ff3590fbc02cc5b34259c4aa748c21
somil kumar <somil.kumar91@gmail.com>
2017-11-05 14:43:22 +0530

    Passing data from command line

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ console.log('Starting app.js');
 const fs = require('fs');
 const _  = require('lodash');
 const notes = require('./notes.js');
+
+var command  = process.argv[2];
+
+console.log('Command :', command);
+
+if (command === 'add') {
+    console.log('Adding a new note');
+} else if(command === 'list'){
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading a note');
+} else if (command === 'remove') {
+    console.log('Removing note');
+} else {
+    console.log('Invalid Command');
+}
