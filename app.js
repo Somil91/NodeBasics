c876f30320fb8aae678b256009c88e9ee88618d7
somil kumar <somil.kumar91@gmail.com>
2017-11-05 14:15:16 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+// let user = os.userInfo();
+// console.log('User Details', user);
+
+// fs.appendFile('greetings.txt', `Hello  ${user.username}! You are ${notes.age} `);
