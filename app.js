30220ce58f85e30bd8e6158b93c4b10cf209ac34
somil kumar <somil.kumar91@gmail.com>
2017-11-05 14:27:09 +0530

    Adding lodash

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _  = require('lodash');
 const notes = require('./notes.js');
+
+var filteredArray = _.uniq([1, 3 , 4, 5, 4, 4]);
+console.log(filteredArray);
+// console.log(_.isString('Somil'));
 
 // let user = os.userInfo();
 // console.log('User Details', user);
